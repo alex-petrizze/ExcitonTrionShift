f05b3a48a09b626faf07ec24144a01e27484bb80
alex-petrizze <u0875477@utah.edu>
2026-05-01 11:20:35 -0600

    Added get filters and add parameter to filter range scroller and filter range

--- Plotter/FilterRange.py
+++ Plotter/FilterRange.py
@@ -6,6 +6,8 @@ from .PlotFunctions import histogram
 from .DataLoader import load_data
 from .AxisRange import AxisRange
 from .BinTextEdit import BinTextEdit
+from PySide6.QtWidgets import QSizePolicy
+import numpy as np
 
 class FilterRange(QWidget):
     def __init__(self, parameter='Parameter'):
@@ -13,7 +15,27 @@ class FilterRange(QWidget):
 
         self.layout = QHBoxLayout()
 
+        self.parameter = parameter
+
+        self.label = QLabel(parameter)
+        self.layout.addWidget(self.label)
+
         self.parameter_range = AxisRange()
-        self.layout.add(self.parameter_range)
+        self.layout.addWidget(self.parameter_range)
+
+        self.delete_button = QPushButton('Delete')
+        self.layout.addWidget(self.delete_button)
 
         self.setLayout(self.layout)
+
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+    def get_filter_info(self):
+        low = self.parameter_range.low
+        high = self.parameter_range.high
+
+        return {
+            'PARAMETER': self.parameter,
+            'MIN': low if low is not None else -np.inf,
+            'MAX': high if high is not None else np.inf
+        }
--- Plotter/FilterRangeScroller.py
+++ Plotter/FilterRangeScroller.py
@@ -6,6 +6,7 @@ from .PlotFunctions import histogram
 from .DataLoader import load_data
 from .AxisRange import AxisRange
 from .BinTextEdit import BinTextEdit
+from .FilterRange import FilterRange
 
 class FilterRangeScroller(QWidget):
     def __init__(self):
@@ -13,15 +14,40 @@ class FilterRangeScroller(QWidget):
 
         self.layout = QVBoxLayout()
 
-
         self.parameter_choice = AxisParameter()
         self.layout.addWidget(self.parameter_choice)
         self.add_parameter_button = QPushButton('Add Filter')
+        self.add_parameter_button.pressed.connect(self.add_parameter)
         self.layout.addWidget(self.add_parameter_button)
 
-        self.scroller = QScrollArea()
-        self.layout.addWidget(self.scroller)
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_container = QWidget()
+        self.scroll_layout = QVBoxLayout(self.scroll_container)
+        self.scroll_area.setWidget(self.scroll_container)
+        self.layout.addWidget(self.scroll_area)
 
         self.setLayout(self.layout)
+
+    def add_parameter(self):
+        parameter = self.parameter_choice.value()
+        
+        print(parameter)
+
+        new_filter_range = FilterRange(parameter)
+        self.scroll_layout.addWidget(new_filter_range)
+
+        print(self.get_filters)
+
+    def get_filters(self):
+        filters = []
+        for i in range(self.scroll_layout.count()):
+            item = self.scroll_layout.itemAt(i)
+            widget = item.widget()
+            
+            if widget is not None:
+                filters.append(widget.get_filter_info())
+
+        return filters
 
     
